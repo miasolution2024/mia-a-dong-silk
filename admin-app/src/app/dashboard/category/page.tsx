0fd0a15990de3ea57eaf557e4242540b99b5e19f
Hieu Vo Trung <hieu.vo@idealogic.com.vn>
2025-08-04 17:22:51 +0700

    update something

--- admin-app/src/app/dashboard/category/page.tsx
+++ admin-app/src/app/dashboard/category/page.tsx
@@ -5,6 +5,7 @@ import { CategoryListView } from "@/sections/category/view/category-list-view";
 
 export const metadata = { title: `Category list | Dashboard - ${CONFIG.appName}` };
 
+
 export default function Page() {
   return <CategoryListView />;
 }
